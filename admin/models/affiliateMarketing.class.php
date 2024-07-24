5a70ee9862186cf6e5e5ecea246c16a5a16c2f1e
Muhammad Saqlain <msaqlain955@gmail.com>
2024-07-24 13:18:04 +0500

    Changed path for entire website

--- admin/models/affiliateMarketing.class.php
+++ admin/models/affiliateMarketing.class.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if (!defined('BASE_URL')) define('BASE_URL', 'http://localhost/verifiedsavers.com/');
+if (!defined('BASE_URL')) define('BASE_URL', 'http://localhost/Affiliate-Marketing-System/');
 
 class affiliateMarketing{
     private $connect;
